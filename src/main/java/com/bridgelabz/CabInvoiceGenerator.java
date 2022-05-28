a6ef236fe2135d46d59ddc23c8467e2a456081a3
SSK137 <saurabhkodam@gmail.com>
2022-05-28 21:25:01 +0530

    Added Fare Calculations

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,4 +1,14 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
+        private static  int costPerMinute = 1;
+        private static  double minimumCostPerKilometer = 10;
+        private static  double minimumFare = 5;
+        public double CalculateFare(double distance, int time) {
+            double totalFare = distance * minimumCostPerKilometer + time * costPerMinute;
+            if (totalFare < minimumFare) {
+                return minimumFare;
+            }
+            return totalFare;
+        }
 }
