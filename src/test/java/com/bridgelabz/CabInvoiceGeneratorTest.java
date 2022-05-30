4555a1a9657ec6c86ad53a83a29bac773bc63895
SSK137 <saurabhkodam@gmail.com>
2022-05-31 01:45:20 +0530

    Added User Invoice Summary

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -3,48 +3,48 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class CabInvoiceGeneratorTest {
     @Test
-    public void GivenDistanceAndTime_ShouldReturnTotalFare()
-    {
-        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+    public void GivenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.CalculateFare(distance, time);
-        double expected = 25;
-        Assertions.assertEquals(expected, fare);
+        double fare = cabInvoiceGenerator.CalculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
     }
 
     @Test
-    public void GivenLessDistanceAndTime_ShouldReturnMinFare()
-    {
-        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+    public void givenLessDistanceAndTime_shouldReturnMinFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.CalculateFare(distance, time);
-        double expected = 5;
-        Assertions.assertEquals(expected, fare);
+        double fare = cabInvoiceGenerator.CalculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+
     }
 
     @Test
-    public void GivenMultipleRides_ShouldReturnTotalFare() {
-        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+    public void givenMultipleRide_ShouldReturnInvoiceSummary() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         Rides[] rides = {new Rides(2.0, 5),
-                new Rides(0.1, 1)
+                new Rides(0.11, 1)
         };
-        InvoiceSummary fare = invoiceGenerator.CalculateFare(rides);
-        double expected = 30;
-        Assertions.assertEquals(expected, fare);
-    }
-
-    @Test
-    public void givenMultipleRide_ShouldReturnInvoiceSummary(){
-        CabInvoiceGenerator invoiceGerator = new CabInvoiceGenerator();
-        Rides[] rides = {new Rides(2.0, 5),
-                new Rides(0.11,1)
-        };
-        InvoiceSummary summary = invoiceGerator.CalculateFare(rides);
+        InvoiceSummary summary = cabInvoiceGenerator.CalculateFare(rides);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-        Assertions.assertEquals(expectedInvoiceSummary,summary);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
+    }
+    @Test
+    public void GivenDistanceAndTimeForUserId_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        String userId = "SSK";
+        Rides[] rides = {new Rides(2.0, 5),
+                new Rides(0.11, 1)
+        };
+        UserID.AddRide(userId, List.of(rides));
+        InvoiceSummary summary = cabInvoiceGenerator.CalculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 }
